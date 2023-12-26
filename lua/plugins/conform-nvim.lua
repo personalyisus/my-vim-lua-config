c300f55b7182241c639be97a30c06e5afb09b825
Jesús Marín <marinalvjesus@gmail.com>
2023-12-26 13:44:16 -0300

    feat: making prettier format all the file formats

--- lua/plugins/conform-nvim.lua
+++ lua/plugins/conform-nvim.lua
@@ -6,7 +6,7 @@ return {
       -- Conform will run multiple formatters sequentially
       python = { "isort", "black" },
       -- Use a sub-list to run only the first available formatter
-      javascript = { { "prettierd", "prettier" } },
+      ['*'] = { { "prettierd", "prettier" } },
     },
   },
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,10 +1,9 @@
-return
-{
+return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",   -- not strictly required, but recommended
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
   cmd = "Neotree",
@@ -59,7 +58,7 @@ return
     local events = require("neo-tree.events")
     opts.event_handlers = opts.event_handlers or {}
     vim.list_extend(opts.event_handlers, {
-      { event = events.FILE_MOVED,   handler = on_move },
+      { event = events.FILE_MOVED, handler = on_move },
       { event = events.FILE_RENAMED, handler = on_move },
     })
     require("neo-tree").setup(opts)
