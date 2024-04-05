e048132310eff70efa09a28be9e3db9623e89c3a
Jesús Marín <marinalvjesus@gmail.com>
2024-04-05 00:14:28 -0300

    fix: actually calling the setup function for the lsp servers that are installed via mason

--- lua/plugins/lsp-conf.lua
+++ lua/plugins/lsp-conf.lua
@@ -1,5 +1,3 @@
--- yisustodo: I definitely can simplify the way the setup
--- settings get passed to each server's setup/config
 return {
   "neovim/nvim-lspconfig",
   event = "BufEnter",
@@ -56,12 +54,40 @@ return {
         end,
       },
       lua_ls = {
-        mason = false,
+        mason = true,
         -- mason = false, -- set to false if you don't want this server to be installed with mason
         -- Use this to add any additional keymaps
         -- for specific lsp servers
         ---@type LazyKeysSpec[]
         -- keys = {},
+
+        -- Taken from https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+        on_init = function(client)
+          local path = client.workspace_folders[1].name
+          if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+            return
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+            runtime = {
+              -- Tell the language server which version of Lua you're using
+              -- (most likely LuaJIT in the case of Neovim)
+              version = "LuaJIT",
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+                -- Depending on the usage, you might want to add additional paths here.
+                -- "${3rd}/luv/library"
+                -- "${3rd}/busted/library",
+              },
+              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+              -- library = vim.api.nvim_get_runtime_file("", true)
+            },
+          })
+        end,
         settings = {
           Lua = {
             workspace = {
@@ -139,8 +165,6 @@ return {
     local ensure_installed = {} ---@type string[]
     for serverName, server_opts in pairs(servers) do
       if server_opts then
-        server_opts = server_opts == true and {} or server_opts
-        -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
         if server_opts.mason == false or not vim.tbl_contains(all_mslp_servers, serverName) then
           defaultSetupFunction(serverName)
         else
@@ -150,7 +174,14 @@ return {
     end
 
     if have_mason then
-      mlsp.setup({ ensure_installed })
+      mlsp.setup({
+        ensure_installed,
+        handlers = {
+          function(serverName)
+            defaultSetupFunction(serverName)
+          end,
+        },
+      })
     end
   end,
 }
