d816fd9d90b64348ba60b4a141bbaf24f8caea9a
Jesús Marín <marinalvjesus@gmail.com>
2024-04-17 14:33:18 -0400

    feat: allowing me to adhd my way into doin kjkjkjkjkj

--- lua/plugins/hardtime-nvim.lua
+++ lua/plugins/hardtime-nvim.lua
@@ -1,5 +1,5 @@
 return {
   "m4xshen/hardtime.nvim",
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  opts = {},
+  opts = { allow_different_key = true, disabled_keys = {} },
 }
