601ac6626dee301e867e41a59a9cdb21af79b51b
Jesús Marín <marinalvjesus@gmail.com>
2024-06-07 22:01:16 -0400

    feat: more adjustments to my conform formatters configuration

--- lua/plugins/conform-nvim.lua
+++ lua/plugins/conform-nvim.lua
@@ -2,15 +2,17 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      lua = { "stylua" },
+      lua = { "stylua", "lua_ls" },
       rust = { "rustfmt" },
       -- Conform will run multiple formatters sequentially
       -- Use a sub-list to run only the first available formatter
-      ["javascript"] = { "prettier" },
-      ["javascriptreact"] = { "prettier" },
-      ["javascript.jsx"] = { "prettier" },
-      ["typescript"] = { "prettier" },
-      ["typescriptreact"] = { "prettier" },
+      ["javascript"] = { "prettier", "eslint_d" },
+      ["javascriptreact"] = { "prettier", "eslint_d" },
+      ["javascript.jsx"] = { "prettier", "eslint_d" },
+      ["typescript"] = { "prettier", "eslint_d" },
+      ["typescriptreact"] = { "prettier", "eslint_d" },
+      ["jsonc"] = { "fixjson" },
+      ["json"] = { "fixjson" },
       ["astro"] = { "prettier" },
       ["scss"] = { "prettier" },
       ["html"] = { "prettier" },
