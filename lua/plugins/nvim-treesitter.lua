0677fa0d5e6629b2ed94d23f915acc1fbbcab9fb
Jesús Marín <marinalvjesus@gmail.com>
2024-08-27 00:33:18 -0400

    feat: disabling treesitter.nvim in the case of huge files

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -13,7 +13,14 @@ return {
       enable = true,
 
       -- List of language that will be disabled
-      disable = {},
+      -- disable = {},
+      disable = function(_, buf)
+            local max_filesize = 200 * 1024 -- 200 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
 
       -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
