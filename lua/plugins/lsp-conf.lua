64c4fdaefdf4f2d0770f5d44afd4620b9b14041f
Jesús Marín <marinalvjesus@gmail.com>
2024-12-18 20:25:22 -0300

    feat: enabling native inlay hints

--- lua/plugins/lsp-conf.lua
+++ lua/plugins/lsp-conf.lua
@@ -20,12 +20,6 @@ return {
       },
       severity_sort = true,
     },
-    -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
-    -- Be aware that you also will need to properly configure your LSP server to
-    -- provide the inlay hints.
-    inlay_hints = {
-      enabled = false,
-    },
     -- add any global capabilities here
     capabilities = {},
     -- Automatically format on save
