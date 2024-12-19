e914c8d8f8312e0c22de4f999e32f1a9b49ee9dc
Jesús Marín <marinalvjesus@gmail.com>
2024-12-18 21:50:26 -0300

    feat: using lsp stuff from mason plugin file directly

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,31 +1,72 @@
 return {
-  "williamboman/mason.nvim",
-  cmd = "Mason",
-  keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-  build = ":MasonUpdate",
-  opts = {
-    ensure_installed = {
-      "stylua",
-      "shfmt",
-      -- "flake8",
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    build = ":MasonUpdate",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shfmt",
+        -- "flake8",
+      },
     },
   },
-  ---@param opts MasonSettings | {ensure_installed: string[]}
-  config = function(_, opts)
-    require("mason").setup(opts)
-    local mr = require("mason-registry")
-    local function ensure_installed()
-      for _, tool in ipairs(opts.ensure_installed) do
-        local p = mr.get_package(tool)
-        if not p:is_installed() then
-          p:install()
-        end
-      end
-    end
-    if mr.refresh then
-      mr.refresh(ensure_installed)
-    else
-      ensure_installed()
-    end
-  end,
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      handlers = {
+        -- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn't have
+        -- a dedicated handler.
+        function(server_name) -- default handler (optional)
+          require("lspconfig")[server_name].setup({})
+        end,
+        ["lua_ls"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.lua_ls.setup({
+            settings = {
+              Lua = {
+                runtime = {
+                  version = "LuaJIT",
+                },
+                workspace = {
+                  checkThirdParty = false,
+                  library = {
+                    vim.env.VIMRUNTIME,
+                  },
+                },
+                completion = {
+                  callSnippet = "Replace",
+                },
+                diagnostics = {
+                  globals = { "vim" },
+                },
+              },
+            },
+          })
+        end,
+        ["eslint"] = function()
+          local lspconfig = require("lspconfig")
+
+          lspconfig.eslint.setup({
+            on_attach = function(client)
+              client.server_capabilities.documentFormattingProvider = false
+            end,
+          })
+        end,
+        -- commenting out tsserver since it is not available for mason for some reason?
+        -- ["tsserver"] = function()
+        --   local lspconfig = require("lspconfig")
+        --   lspconfig.tsserver.setup({
+        --     on_attach = function(client)
+        --       client.server_capabilities.documentFormattingProvider = true
+        --     end,
+        --   })
+        -- end,
+        -- instead trying out vtsls
+      },
+    },
+  },
+  "neovim/nvim-lspconfig",
 }
